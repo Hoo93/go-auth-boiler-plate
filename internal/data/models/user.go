7ba869209d89c38443553fa5e3b763c8faa00872
Sang Hoo Park <sksk8922@gmail.com>
2025-04-24 14:07:15 +0900

    feat: TableName 명시

--- internal/data/models/user.go
+++ internal/data/models/user.go
@@ -1,11 +1,11 @@
 package models
 
-import "gorm.io/gorm"
-
 type User struct {
-	gorm.Model
-	ID       uint   `gorm:"primaryKey"`
+	ID       int32  `gorm:"primaryKey"`
 	UserName string `gorm:"column:userName"`
 	Email    string `gorm:"column:email"`
 	Password string `gorm:"column:password"`
 }
+
+// TableName ::
+func (User) TableName() string { return "User" }
